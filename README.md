068856b119430dd08291a4e4d74c70b1d3745ef6
Roberto Villalobos Murillo <robertovillalobosml@gmail.com>
2023-03-11 20:32:33 -0600

    correction on README.md file

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
-## Lorenz system of equation
+## Lorenz system of differential equations also called the Lorenz strange attractor
 
 This is an implementation and optimization of the code presented by the user Hevenicio [here](https://github.com/Hevenicio/3D-Lorenz-Attractor-simulation-with-python). The present version of this code was writted (except for the english comments) as part of a project for my differential equations course back in 2021; the investigation project(in spanish) can be found as one of the files.
 
-The differences between this code and the one that served as inspiration are basicaly 2:
-1. I'm plotting several solutions with different initial conditions to explicitely show the sensitivity to initial conditions of this caotic system.
-2. I'm plotting every "trajectory" only once and then i update the code in every step instead of plotting in every step.
+The differences between this code and the one that served as inspiration are basicaly two:
+1. I'm plotting several solutions with different initial conditions to explicitly show the sensitivity to initial conditions of this caotic system.
+2. I'm plotting every "trajectory" only once and then updating the code in every step instead of plotting in every step. The tests made show that this way represents a consirable gain in the speed of the animation.
 
-An explanation of the basic theory concerning this system of equation can be found in the pdf file.
+An explanation of the basic theory concerning this system of diffential equations can be found in the pdf file.
--- lorenz_animation_final.py
+++ lorenz_animation_final.py
@@ -17,13 +17,10 @@ def lorenz_eq(variables, t, beta = 8/3, sigma = 10 , rho = 28):
 
 # some possible initial conditions 
 #st0 = [0, 1, 1.05]
-
 st0 = [1,1,20]
 st1 = [1.3,1.04,20]
-
 st2 = [1.7,1.8,19.86]
 st3 = [2, 1.34, 20 + 10/33]
-
 # st0= [60,65,10]
 # st1 = [-50,30,6]
 
@@ -31,20 +28,15 @@ t0 = np.linspace(0, 100, 20000)
 n = len(t0)
 
 #solutions for the several initial conditions and the same time interval
-
 sol0 = odeint(lorenz_eq, st0, t0)
 sol1 = odeint(lorenz_eq, st1, t0)
 sol2 = odeint(lorenz_eq, st2, t0)
 sol3 = odeint(lorenz_eq, st3, t0)
 
 #initialization and settings for the figure and the plots
-
 fig = plt.figure('Soluciones en tiempo real',figsize=(8, 6), dpi = 100)
-
-
 ax1 = fig.add_subplot(111, projection='3d')
 fig.subplots_adjust(top = 0.914, bottom= 0, right= 0.855)
-
 
 p0, = ax1.plot([], [], [], color='#57D702', linewidth=0.78, alpha = 0.9) #verde
 p1, = ax1.plot([], [], [], color = '#AD2929', linewidth = 0.78, alpha = 0.7) #rojo
@@ -52,7 +44,6 @@ p2, = ax1.plot([], [], [], color='#2045CF', linewidth=0.78, alpha = 0.5) #azul
 p3, = ax1.plot([], [], [], color='#F39C12', linewidth=0.78, alpha = 0.4) #~mostaza
 
 #the next function initializes the animation
-
 def init_func():
 
     ax1.set_xlim3d(-30,30)
@@ -63,11 +54,8 @@ def init_func():
     # ax.set_zlabel("z")
     ax1.set_axis_off()
 
-
-
 #the next function updates the plots in every moment of time t of t0
 def animate(i):
-
 # this next lines rotates the animation
     ax1.view_init(elev=5,azim = i/4+1)
 
@@ -84,7 +72,6 @@ def animate(i):
     p3.set_data(sol3.T[0][:i], sol3.T[1][:i])
     p3.set_3d_properties(sol3.T[2][:i])
 
-
 #a very fast interval of time: 1/(2**64), 
 #some decent time intervals are: 5,8,10 (greater than 15 makes the animation too slow)
 
@@ -96,6 +83,5 @@ anim = FuncAnimation(fig,
                      init_func = init_func(),
                      repeat = False,
                      cache_frame_data = False)
-
 
 plt.show()
